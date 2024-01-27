dbaa8ccc5ae9bdd393992eef20a25a50fb9261c5
09016.cyril.rejas <09016.cyril.rejas@ict888.net>
2024-01-27 17:12:55 +0800

    oho hoh

--- tf-code-aws/prod/us-east-1/vpc/main.tf
+++ tf-code-aws/prod/us-east-1/vpc/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "vpc" {
   source  = "app.terraform.io/cyrej_yt_ops/vpc/aws"
   version = "1.0.2"
-  vpc_configs_map = var.vpc_configs
+  vpc_configs = var.vpc_configs
 }
 
 
